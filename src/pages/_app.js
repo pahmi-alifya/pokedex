c6820513405850d94ee4890c2d5ab38d1cd6eac2
Pahmi Alifya Bahri <pahmi.bahri@mncgroup.com>
2023-02-28 20:09:40 +0700

    update: config for apollo client

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { offsetLimitPagination } from "@apollo/client/utilities";
+
+const client = new ApolloClient({
+  uri: "https://graphqlpokemon.favware.tech/v7",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          getAllPokemon: offsetLimitPagination(),
+        },
+      },
+    },
+  }),
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
